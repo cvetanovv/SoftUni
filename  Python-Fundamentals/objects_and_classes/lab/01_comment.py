e704a638ac2bab76d5e0cd23b7b15d9dd1b63d5f
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2021-10-25 17:38:26 +0100

    Add exercise

---
+++  Python-Fundamentals/objects_and_classes/lab/01_comment.py
@@ -0,0 +1,11 @@
+class Comment:
+    def __init__(self, username, content, likes=0):
+        self.username = username
+        self.content = content
+        self.likes = likes
+
+
+comment = Comment('Ivan', 'I like this book', 10)
+print(comment.username)
+print(comment.content)
+print(comment.likes)
---
+++  Python-Fundamentals/objects_and_classes/lab/02._party.py
@@ -0,0 +1,19 @@
+class Party:
+
+    def __init__(self):
+        self.people = []
+
+
+
+my_party = Party()
+total_people = 0
+
+while True:
+    name = input()
+    if name == "End":
+        break
+    my_party.people.append(name)
+    total_people += 1
+
+print(f"Going: {', '.join(my_party.people)}")
+print(f"Total: {total_people}")
