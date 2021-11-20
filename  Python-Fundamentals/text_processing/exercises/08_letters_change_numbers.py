80809cc3044a41f13dec7875aba287b69b876997
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2021-11-20 17:08:26 +0000

    Add exercise

---
+++  Python-Fundamentals/text_processing/exercises/08_letters_change_numbers.py
@@ -0,0 +1,23 @@
+import string
+
+list_of_strings = [text.strip() for text in input().split()]
+total_sum = 0
+
+for text in list_of_strings:
+    number = ""
+    temporary_sum = 0
+    for num in text:
+        if num.isdigit():
+            number += num
+    if text[0].isupper():
+        temporary_sum += (int(number) / (string.ascii_uppercase.index(text[0]) + 1))
+    elif text[0].islower():
+        temporary_sum += (int(number) * (string.ascii_lowercase.index(text[0]) + 1))
+    if text[-1].isupper():
+        temporary_sum -= (string.ascii_uppercase.index(text[-1]) + 1)
+    elif text[-1].islower():
+        temporary_sum += (string.ascii_lowercase.index(text[-1]) + 1)
+    total_sum += temporary_sum
+
+print(f"{total_sum:.2f}")
+# print(string.ascii_uppercase.index("G") + 1)
