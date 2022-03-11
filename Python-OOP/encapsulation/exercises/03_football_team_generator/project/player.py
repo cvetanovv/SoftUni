5d19b06fe922545511652cc9ecd1ad20bc70f450
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-03-11 15:17:06 +0200

    Add exercises

---
+++ Python-OOP/encapsulation/exercises/03_football_team_generator/project/player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, name, sprint, dribble, passing, shooting):
+        self.__name = name
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting
+
+    @property
+    def name(self):
+        return self.__name
+
+    def __str__(self):
+        result = f"Player: {self.__name}\n Sprint: {self.__sprint}\n Dribble: {self.__dribble}\n Passing: {self.__passing}\n Shooting: {self.__shooting}"
+        return result
