aebca3db710fc227c29f597627ae817e032ae2de
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-06-12 17:20:33 +0300

    Added classes exercises

--- JavaScript-Advanced/classes/lab/02-get-persons.js
+++ JavaScript-Advanced/classes/lab/02-get-persons.js
@@ -1,25 +1,23 @@
-class Person {
-    constructor(firstName, lastName, age, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.email = email;
+function returnsArrayOfPerson() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
     }
 
-    returnPerson() {
-        let returnedPerson = this.firstName.map()
-    } 
+    let anna = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let softUni = new Person('SoftUni');
+    let stephan = new Person('Stephan', 'Johnson', 25);
+    let gabriel = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    return [anna, softUni, stephan, gabriel];
 }
 
-function foo () {
-    const data = [
-        ['Anna', 'Simpson', 22, 'anna@yahoo.com'],
-        ['SoftUni'],
-        ['Stephan', 'Johnson', 25],
-        ['Gabriel', 'Peterson', 24, 'g.p@gmail.com'],
-    ]
-
-    return data.map(x => new Person(...x))
-}
-
-console.log(foo())
+console.log(returnsArrayOfPerson());
